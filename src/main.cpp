b75bcc73349351881993c82fba4d76b697169e81
Vitry Hugo <hugo.louis.vitry@gmail.com>
2022-12-28 20:51:28 +0100

    alegrement copié et réadapté de jeremy dummmy bot

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,23 @@
 #include "motor.h"
 #include "diff_control.h"
 
-int motorA_PWM      = 3 ;
-int motorB_PWM      = 5;
-int motorA_dir_pos  = 11;
-int motorB_dir_pos  = 8 ;
-int motorA_dir_neg  = 10;
-int motorB_dir_neg  = 6 ;
 
-Motor motor_A(motorA_PWM,motorA_dir_pos,motorA_dir_neg);
-Motor motor_B(motorB_PWM,motorB_dir_pos,motorB_dir_neg);
+int motor_1_A  = 13;
+int motor_1_B  = 12;
+int motor_2_A  = 27;
+int motor_2_B  = 14;
 
-Controll controller(motor_A,motor_B);
+bool FORWARD = true;
+bool TRIGO = true;  
+
+Motor motor_1(motor_1_A,motor_1_B);
+Motor motor_2(motor_2_A,motor_2_B);
+
+Controll controller(motor_1,motor_2);
 
 void setup()
 {
-    Serial.begin(9600);
-    pinMode(13,OUTPUT);    
-
+    Serial.begin(9600); 
 }
 
 void loop()
@@ -28,13 +28,10 @@ void loop()
 
     if(-1!=cmd)
     {
-        //Serial.print("Received order :");
-        Serial.println(cmd);
-        digitalWrite(13,HIGH);
-        if('F' == cmd ){controller.move(NOMINAL_MOVE_SPEED);}
-        else if('B' == cmd ){controller.move(-NOMINAL_MOVE_SPEED);}
-        else if('L' == cmd ){controller.rotate(NOMINAL_ROTATION_SPEED);}
-        else if('R' == cmd ){controller.rotate(-NOMINAL_ROTATION_SPEED);}
+        if('F' == cmd ){controller.move(FORWARD);}
+        else if('B' == cmd ){controller.move(!FORWARD);}
+        else if('L' == cmd ){controller.rotate(TRIGO);}
+        else if('R' == cmd ){controller.rotate(!TRIGO);}
         else if('S' == cmd ){controller.stop();}
         //Serial.println("Executed");
 
